05bdb7e4905a11127f96df69002fe17d342deb70
LuisMiRey <lmromano1879@gmail.com>
2023-01-20 15:32:29 +0100

    classes practices

---
+++ Basic-Python/OOP/classes.py
@@ -0,0 +1,27 @@
+import random 
+
+class Dog:
+    info = "A domesticated carnivorous mammal"
+
+    def __init__(self,name,age):
+        print("I am alive!")
+        self.lucky_number = random.randint(1,10)
+        self.name = name
+        self.age = age
+        
+dog1 = Dog("Rodolf",20)
+dog2 = Dog("Ketty",25)
+
+#print(dog1.lucky_number)
+#print(dog2.lucky_number)
+
+#dog1.name = "Lukas"
+
+#print(dog1.name + " and " + dog1.age)
+#print(dog2.name + " and " + dog2.age)
+print(dog1.name)
+print(dog2.name)
+print(dog1.age)
+print(dog2.age)
+# print(dog3.name) name attribute is only creatred to previous object.
+
