b10e005163a1c55628c1776894c66894eaf53bcd
LuisMiRey <lmromano1879@gmail.com>
2023-02-18 21:29:24 +0100

    inheritance

---
+++ Basic-Python/OOP/Inheritance.py
@@ -0,0 +1,44 @@
+import random
+
+class Animal:
+    info = "A living organis, that feeds on organic matter"
+
+    def __init__(self, name):
+        print("An animal is created")
+        self.name = name
+
+class Dog(Animal):
+    info = "a domesticated carnivorous mammal that typically has a long snout"
+
+    def __init__(self, name):
+        super().__init__(name)
+        print("A dog is created")
+        self.lucky_number = random.randint(1,10)
+        self.fur = ""
+
+
+    def bark(self):
+        print(f"Wolf! My name is {self.name} and my number is {self.lucky_number}")
+
+class Bulldog(Dog):
+
+    def __init__(self,name):
+        super().__init__(name)
+        print("A Bulldog is created")
+
+class Pet(Bulldog):
+
+    def __init__(self,name):
+        super().__init__(name)
+        print("It is my pet")
+
+
+dog1 = Pet("Fido")
+dog1.bark()
+
+''' dog1 = Dog("Fido")
+#dog2 = Animal()
+
+#print(dog2.info)
+print(dog1.info) '''
+
