cb9999a2f65f56849d2b88532d6d24abab7ae5ad
LuisMiRey <lmromano1879@gmail.com>
2023-03-26 23:20:40 +0200

    lambda function

--- Basic-Python/OOP/map.py
+++ Basic-Python/OOP/map.py
@@ -8,13 +8,20 @@ class Student:
 student = [Student("Andres", 0.46), Student("Naomi", 0.75), Student("Musk", 0.95)]
 
 student_results = []
-for student in students:
+for student in student:
 #    if student.score >= 0.7:
 #        student_results.append(f"{student.name} Passed")
 #    else:
 #        student_results.append(f"{student.name} Failed")
      student_results.append(f"{student.name} Passed") if student.score >= 0.7 else student_results.append(f"{student.name} Failed")
 
-map(lambda student: student.name, students)
+#map(lambda student: student.name, students)
 
 print(student_results)
+
+#Another form
+
+numbers = [1,2,3,4,5]
+
+print(list(map(lambda number: number * 2, numbers)))
+
